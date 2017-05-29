57c7c148515ddb7cc3dc1cb8cc28a7b3b2d4ab23
Tiago Ferreira <tiagof7@yahoo.com.br>
2017-05-29 11:44:43 -0300

    Code Identation

--- api/public-ip.js
+++ api/public-ip.js
@@ -9,35 +9,35 @@ function publicIP(context) {
 /* 
  * GET - List available public IP addresses in a space
  */
-publicIP.prototype.getAll = function(options) {
+publicIP.prototype.getAll = function (options) {
     return commons.requestWrapper(this.ctx.getContainersEndpoint(options.region) + "/containers/floating-ips", this, options);
 }
 
 /* 
  * POST - Bind a public IP address to a single container
  */
-publicIP.prototype.get = function(options) {
+publicIP.prototype.get = function (options) {
     return commons.requestWrapper(this.ctx.getContainersEndpoint(options.region) + "/containers/" + options.container + "/floating-ips/" + options.ip + "/bind/", this, options, "POST");
 }
 
 /* 
  * POST - Unbind a public IP address from a container
  */
-publicIP.prototype.get = function(options) {
+publicIP.prototype.get = function (options) {
     return commons.requestWrapper(this.ctx.getContainersEndpoint(options.region) + "/containers/" + options.container + "/floating-ips/" + options.ip + "/unbind/", this, options, "POST");
 }
 
 /* 
  * POST - Request a public IP address for a space
  */
-publicIP.prototype.get = function(options) {
+publicIP.prototype.get = function (options) {
     return commons.requestWrapper(this.ctx.getContainersEndpoint(options.region) + "/containers/floating-ips/request", this, options, "POST");
 }
 
 /* 
  * POST - Release public IP address
  */
-publicIP.prototype.get = function(options) {
+publicIP.prototype.get = function (options) {
     return commons.requestWrapper(this.ctx.getContainersEndpoint(options.region) + "/containers/floating-ips/" + options.ip + "/release/", this, options, "POST");
 }
 
